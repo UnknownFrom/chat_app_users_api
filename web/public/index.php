63e3257721fd67f9dbaa13581a945f1a6ab0ca62
UnknownFrom <87531160+UnknownFrom@users.noreply.github.com>
2021-10-20 18:42:46 +0300

    Изменение в index.php

--- web/public/index.php
+++ web/public/index.php
@@ -9,6 +9,10 @@ use pavel\users\ActivationEmail;
 
 include "../app/vendor/autoload.php";
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 try {
     Routes::init();
     $parameters = Routes::getParameters();
